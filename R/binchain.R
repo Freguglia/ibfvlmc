b2f069fbea68fcaac866f383d8353879ab10b331
Freguglia <victorfreguglia@gmail.com>
2021-09-19 15:12:56 -0300

    Documents binchain

--- R/binchain.R
+++ R/binchain.R
@@ -1,2 +1,8 @@
-#' List of 4 binary chains
+#' @title Example VLMC data
+#' @description List of 4 binary chains with length 2500 each.
+#' 
+#' @name binchain
+#' @docType data
+#' @author Victor Freguglia
+#' 
 "binchain"
